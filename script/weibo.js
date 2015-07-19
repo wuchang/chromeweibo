64962d50eeb0542983c6cadc53a9c947e602b6fd
wuchang-x230 <wuchangx@gmail.com>
2015-07-20 00:06:59 +0800

    能读取到微博内容

--- script/weibo.js
+++ script/weibo.js
@@ -1,9 +1,29 @@
 
-var arrfeed = $('div.WB_feed>div');
 
-for (var i = 0; i < arrfeed.length; i++) {
-  var feed=arrfeed[i];
-  var author=$('.WB_info',feed).text();
-  console.log('author:' + i +',' + author);
-}
+var scrollCount = 0;
+
+//读取微博内容
+function readWeibo() {
+  var arrfeed = $('div.WB_feed div[mrid]');
+  console.log('weibo count:' + arrfeed.length)
+  for (var i = 0; i < arrfeed.length; i++) {
+    var feed = arrfeed[i];
+    var author = $('.WB_detail', feed).text().replace(/\s/g, '');
  
+    console.log('WB_detail:' + i + ',' + author);
+  }
+}
+
+//滚动窗口
+function scrollWin() {
+  scrollCount++;
+  window.scroll(0, 5000000);
+  console.log('scroll:' + scrollCount)
+  if (scrollCount < 10) {
+    window.setTimeout(scrollWin, 500);
+  } else {
+    readWeibo();
+  }
+}
+
+readWeibo()
